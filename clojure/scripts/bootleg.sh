fb3a7ad75b1d77f95ac67f2243c1a9ab5ac66a23
GitHub Action <action@github.com>
2021-01-06 00:42:30 +0000

    Add changes

--- clojure/scripts/bootleg.sh
+++ clojure/scripts/bootleg.sh
@@ -13,6 +13,7 @@ CURL=`which curl`
 [ -n ${SHASUM} ] && [ -n ${CURL} ] || exit 2
 
 TARGET="https://github.com/retrogradeorbit/bootleg/releases/download/v${VERSION}/bootleg-${VERSION}-windows-amd64.zip"
+AUTOUPDATE="https://github.com/retrogradeorbit/bootleg/releases/download/v\$version/bootleg-\$version-windows-amd64.zip"
 
 CHECKVER_CODE=`curl -X HEAD -m 3 -sfw "%{response_code}" ${TARGET}`
 if [ $CHECKVER_CODE -ne 302 ]; then
@@ -24,18 +25,26 @@ SHA256SUM=$(curl -sLS --fail-early "${TARGET}" | shasum -a 256 -b | cut -f1 -d\ 
 
 cat <<MANIFEST  
 {
+    "version": "${VERSION}",
     "description": "Simple template processing command line tool to help build static websites",
     "homepage": "https://github.com/retrogradeorbit/bootleg",
     "license": "EPL-2.0",
-    "version": "${VERSION}",
+    "depends": "extras/vcredist2010",
     "architecture": {
         "64bit": {
             "url": "${TARGET}",
             "hash": "${SHA256SUM}"
         }
     },
-    "depends": "extras/vcredist2010",
-    "bin": "bootleg.exe"
+    "bin": "bootleg.exe",
+    "checkver": "github",
+    "autoupdate": {
+        "architecture": {
+            "64bit": {
+                "url": "${AUTOUPDATE}"
+            }
+        }
+    }
 }
 
 MANIFEST
